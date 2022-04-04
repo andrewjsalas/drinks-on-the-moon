ff5fd7a8fc9db2e388d4ecc0abe51f4f3204b9dd
asalas <67822688+andrewjsalas@users.noreply.github.com>
2022-04-04 12:22:57 -0700

    Set Routes
    Created all the necessary routes within app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
-
+import Home from "./pages/Home";
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Router>
-      
-    </Router>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={Home} />
+          <Route path='/about' element={About} />
+          <Route path='/cocktail:id' element={SingleCocktail} />
+        </Routes>
+      </Router>
+
+      <ToastContainer />
+    </>
   );
 }
 
