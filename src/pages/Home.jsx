43e2ded85d7a30d2c118e75a86db079501588331
asalas <67822688+andrewjsalas@users.noreply.github.com>
2022-04-04 12:41:59 -0700

    Created home page
    Created the Home page and set up the API call to fetch the information.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import CocktailList from "../components/CocktailList"
+import SearchBar from '../components/SearchBar'
 
 function Home() {
+  const [loading, setLoading] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('a')
+  const [cocktails, setCocktails,] = useState([])
+
+  useEffect(() => {
+    setLoading(true)
+    async function getDrinks() {
+      try {
+        const response = await fetch(
+          `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchTerm}`
+        )
+        const data = await response.json()
+        const { drinks } = data
+        if (drinks) {
+          const newCocktails = drinks.map((item) => {
+            const {
+              idDrink, 
+              strDrink, 
+              strDrinkThumnb,
+              strAlcoholic,
+              strGlass,
+            } = item
+            return {
+              id: idDrink, 
+              name: strDrink,
+              image: strDrinkThumnb,
+              info: strAlcoholic,
+              glass: strGlass,
+            }
+          })
+          setCocktails(newCocktails)
+        } else {
+          setCocktails([])
+        }
+      } catch (error) {
+        console.log(error)
+      }
+      setLoading(false)
+    }
+    getDrinks()
+  }, [searchTerm])
+
   return (
-    <div>Home</div>
+    <main>
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <CocktailList loading={loading} cocktails={cocktails} />
+    </main>
   )
 }
 
